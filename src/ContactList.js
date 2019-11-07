0aa0306cc35c6b0e820d35befe04671fcf37d594
TBM85 <tballesterm@gmail.com>
2019-11-07 12:04:49 -0500

    Sort the contact list and filter the contacts in the search box

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 import SimpleMenu from './SimpleMenu';
 
 class ContactList extends Component {
+  state = {
+    query: ""
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query.trim()
+    })
+  }
+
   render() {
     const { contacts, removeContact } = this.props;
+    const { query } = this.state;
+
+    let showContacts;
+    const match = new RegExp(escapeRegExp(query), 'i');
+
+    (query) ?
+      showContacts = contacts.filter((contact) => match.test(contact.name))
+    :
+      showContacts = contacts
+
+    showContacts.sort(sortBy('name'));
 
     return(
       <section id="contacts">
@@ -12,12 +35,14 @@ class ContactList extends Component {
             className="contact-search"
             type="text"
             placeholder="Search contacts"
+            value={query}
+            onChange={(event) => this.updateQuery(event.target.value)}
           />
           <a href="#" className="contact-add">Add Contact</a>
         </div>
 
         <ol className="contact-list">
-          {contacts.map((contact) => (
+          {showContacts.map((contact) => (
             <li key={contact.id} className="contact-list-item">
               <div className="contact-avatar" style={{backgroundImage: `url(${contact.avatar}`}} />
               <div className="contact-details">
