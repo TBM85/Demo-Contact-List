8af0137f3d981b71186b39ae309d967fc60398a3
TBM85 <tballesterm@gmail.com>
2020-09-21 09:25:42 -0400

    Updates

---
+++ precache-manifest.8246d1899c223f4acf42d80ea239ae60.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09432f6f3325f7abcb9cb95bc7064451",
+    "url": "/Demo-Contact-List/index.html"
+  },
+  {
+    "revision": "c368229673c76450d044",
+    "url": "/Demo-Contact-List/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "0f2d71161907724c4e81",
+    "url": "/Demo-Contact-List/static/css/main.1a2f9d79.chunk.css"
+  },
+  {
+    "revision": "c368229673c76450d044",
+    "url": "/Demo-Contact-List/static/js/2.b13dea19.chunk.js"
+  },
+  {
+    "revision": "0f2d71161907724c4e81",
+    "url": "/Demo-Contact-List/static/js/main.1e542656.chunk.js"
+  },
+  {
+    "revision": "494add3fbcdadbb14408",
+    "url": "/Demo-Contact-List/static/js/runtime-main.f2862ac4.js"
+  },
+  {
+    "revision": "ac9440512ba56b8f58e72384a30b0cb6",
+    "url": "/Demo-Contact-List/static/media/arrow_back.ac944051.svg"
+  },
+  {
+    "revision": "3dc9c6f233c8796c5a5854bcbb8a7a35",
+    "url": "/Demo-Contact-List/static/media/more_vert.3dc9c6f2.svg"
+  },
+  {
+    "revision": "25cabb353b79433fc1bdfc921de120b7",
+    "url": "/Demo-Contact-List/static/media/person.25cabb35.svg"
+  },
+  {
+    "revision": "dab0f086795a2c824127ea4910e94459",
+    "url": "/Demo-Contact-List/static/media/person_add.dab0f086.svg"
+  },
+  {
+    "revision": "8b7b6457eb083cb8e519e63d7067c68e",
+    "url": "/Demo-Contact-List/static/media/photo.8b7b6457.svg"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/Demo-Contact-List/static/media/search.a87eca9e.svg"
+  }
+]);
